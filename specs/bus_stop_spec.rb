707bed8f7740b53a4f57aed9e7af231cbfefb024
Shabana Mirza <shabanamirza2010@hotmail.com>
2018-11-27 15:46:04 +0000

    add bus stop class

---
+++ specs/bus_stop_spec.rb
@@ -0,0 +1,12 @@
+require("minitest/autorun")
+require('minitest/rg')
+require_relative("../bus_stop")
+
+class BusStopTest < MiniTest::Test
+
+  def test_stop_has_name
+    newstop = BusStop.new("Leith Walk")
+    assert_equal(newstop.name, "Leith Walk")
+  end
+
+end
